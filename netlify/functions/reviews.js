8c84b889c3ee60be23510719728a7010842af517
Rahul <rbhat5480@gmail.com>
2024-04-23 11:06:00 +0530

    signup changes and popular

--- netlify/functions/reviews.js
+++ netlify/functions/reviews.js
@@ -21,7 +21,7 @@ const db = client.db('project');
 const reviewsCollection = db.collection('reviews');
 
 // Route handler to fetch reviews based on filter
-app.get('/reviewsbyfilter/:filter', async (req, res) => {
+app.get('/reviews/:filter', async (req, res) => {
     try {
         const filter = req.params.filter;
 
