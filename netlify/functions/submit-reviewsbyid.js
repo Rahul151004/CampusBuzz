e90e1843f2edda5ec9ce4021003200769afb84fb
Rahul <rbhat5480@gmail.com>
2024-04-23 13:17:54 +0530

    Minor changes

--- netlify/functions/submit-reviewsbyid.js
+++ netlify/functions/submit-reviewsbyid.js
@@ -7,7 +7,8 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 exports.handler = async (event, context) => {
     try {
         const requestBody = queryString.parse(event.body);
-        const { id } = event.path.split('/').pop(); // Extract id from the path
+        const path = event.path.replace('/.netlify/functions/', ''); // Extract path without prefix
+        const id = parseInt(path.split('/').pop()); // Extract id from the path
         const { title, reviewtxt, rating } = requestBody;
 
         await client.connect();
@@ -15,7 +16,7 @@ exports.handler = async (event, context) => {
         const db = client.db('project');
         const reviewsCollection = db.collection('reviews');
 
-        await reviewsCollection.insertOne({ facility_id: parseInt(id), title, reviewtxt, rating });
+        await reviewsCollection.insertOne({ facility_id: id, title, reviewtxt, rating });
 
         return {
             statusCode: 200,
