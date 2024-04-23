99454929b8b523016e7d4ed048148187a477ca49
Rahul <rbhat5480@gmail.com>
2024-04-23 13:49:25 +0530

    Minor changes

--- netlify/functions/submit-reviewsbyid.js
+++ netlify/functions/submit-reviewsbyid.js
@@ -16,7 +16,7 @@ exports.handler = async (event, context) => {
         const db = client.db('project');
         const reviewsCollection = db.collection('reviews');
 
-        await reviewsCollection.insertOne({ facility_id: id, title, reviewtxt, rating });
+        await reviewsCollection.insertOne({ facility_id: id, title, timestamp,reviewtxt, rating });
 
         return {
             statusCode: 200,
