cda824967763087fc61be5c008071cd6fb56eb5b
Rahul <rbhat5480@gmail.com>
2024-04-23 11:49:51 +0530

    Minor changes

--- netlify/functions/reviewsbyid.js
+++ netlify/functions/reviewsbyid.js
@@ -1,32 +1,35 @@
-const express = require('express');
 const { MongoClient } = require('mongodb');
-const serverless=require('serverless-http');
-const app = express();
 
 const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-client.connect(err => {
-    if (err) {
-        console.error('Error connecting to MongoDB:', err);
-        return;
-    }
-    console.log('Connected to MongoDB');
-});
-
-const db = client.db('project');
-const reviewsCollection = db.collection('reviews');
-
-app.get('/reviews/:id', async (req, res) => {
+exports.handler = async (event, context) => {
     try {
-        const id = parseInt(req.params.id);
+        await client.connect();
+
+        const db = client.db('project');
+        const reviewsCollection = db.collection('reviews');
+
+        const id = parseInt(event.pathParameters.id);
         const reviews = await reviewsCollection.find({ facility_id: id }).toArray();
-        // Construct HTML response
-        res.send(/* Constructed HTML */);
+
+        return {
+            statusCode: 200,
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(reviews),
+        };
     } catch (error) {
-        console.error('Error fetching reviews:', error);
-        res.status(500).send('Error fetching reviews');
+        console.error('Error:', error);
+        return {
+            statusCode: 500,
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ error: 'Internal server error' }),
+        };
+    } finally {
+        await client.close();
     }
-});
-module.exports.handler = serverless(app);
-module.exports = app;
+};
